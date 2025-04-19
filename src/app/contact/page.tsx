27c875e670d52fd8e37c727da0098b65f605700a
Firdo <100759853+Firadians@users.noreply.github.com>
2025-04-19 12:31:08 +0800

    Add Contact and Price pages with basic structure and content

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+// src/app/contact/page.tsx
+
+export default function ContactPage() {
+    return (
+      <div>
+        <h1>Contact Page</h1>
+        <p>Feel free to reach out to us!</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/price/page.tsx
@@ -0,0 +1,11 @@
+// src/app/contact/page.tsx
+
+export default function PricePage() {
+    return (
+      <div>
+        <h1>Price Page</h1>
+        <p>Feel free to reach out to us!</p>
+      </div>
+    );
+  }
+  
